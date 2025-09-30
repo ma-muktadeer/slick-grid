47be4b8994d756733f7cc0ff62113a30882d8b6a
ma-muktadeer <muktadeer.ma@gmail.com>
2025-10-01 00:05:01 +0600

    ok 2

--- src/app/examples/rowdetail-preload.component.ts
+++ src/app/examples/rowdetail-preload.component.ts
@@ -3,7 +3,7 @@ import { Component, OnDestroy } from '@angular/core';
 @Component({
   template: `<div class="container-fluid d-flex align-items-center" style="margin-top: 10px">
     <i class="mdi mdi-sync mdi-spin mdi-50px"></i>
-    <h4>Loading...</h4>
+    <h4>Loading... ithouse</h4>
   </div>`,
 })
 export class RowDetailPreloadComponent implements OnDestroy {
